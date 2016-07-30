1f2b6389012525076e0a655b65fad28b133731f1
shiva2006 <shiva.rsgr1@gmail.com>
2016-07-30 23:21:46 +0530

    modified for login authentication

--- sps/src/hot/com/mydomain/sps/bean/AuthenticatorBean.java
+++ sps/src/hot/com/mydomain/sps/bean/AuthenticatorBean.java
@@ -1,12 +1,22 @@
 package com.mydomain.sps.bean;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
+
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Logger;
 import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Out;
+import org.jboss.seam.international.StatusMessage.Severity;
+import org.jboss.seam.international.StatusMessages;
 import org.jboss.seam.log.Log;
 import org.jboss.seam.security.Credentials;
 import org.jboss.seam.security.Identity;
+
+import com.mydomain.sps.entities.User;
+import com.mydomain.sps.service.BaseDao;
+import com.mydomain.sps.service.Constants;
 
 @Stateless
 @Name("authenticator")
@@ -16,19 +26,35 @@ public class AuthenticatorBean implements Authenticator
 
     @In Identity identity;
     @In Credentials credentials;
+    
+    @In BaseDao baseDaoImpl;
+    
+    @In @Out User user;
 
     public boolean authenticate()
     {
-        log.info("authenticating {0}", credentials.getUsername());
-        //write your authentication logic here,
-        //return true if the authentication was
-        //successful, false otherwise
-        if ("admin".equals(credentials.getUsername()))
-        {
-            identity.addRole("admin");
-            return true;
+        boolean authFlag = Boolean.FALSE;
+    	if (credentials.getUsername().isEmpty()
+				|| credentials.getPassword().isEmpty()) 
+    	{
+			StatusMessages.instance().add(Severity.ERROR,"Invalid credentials, Please try again");
+			authFlag = Boolean.FALSE;
+		}
+    	else
+    	{
+        	List<User> result = baseDaoImpl.find(Constants.GET_USERQ, credentials.getUsername(), credentials.getPassword());
+        	if (null == result || result.isEmpty()) 
+        	{
+        		StatusMessages.instance().add(Severity.ERROR,"Invalid credentials, Please try again");
+        		authFlag=Boolean.FALSE;
+        	}
+        	else
+        	{
+        		user = result.get(0);
+        		authFlag=Boolean.TRUE;
+        	}
         }
-        return false;
+        return authFlag;
     }
 
 }
