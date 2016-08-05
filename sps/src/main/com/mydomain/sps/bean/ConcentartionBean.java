3198d5ad1b3921333bde51d927861961bd6d4231
shiva2006 <shiva.rsgr1@gmail.com>
2016-08-05 21:50:39 +0530

    Shanthraj--------- working on concentration

---
+++ sps/src/main/com/mydomain/sps/bean/ConcentartionBean.java
@@ -0,0 +1,11 @@
+package com.mydomain.sps.bean;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+
+@Name("concentartionBean")
+@Scope(ScopeType.PAGE)
+public class ConcentartionBean {
+	
+}
---
+++ sps/src/main/com/mydomain/sps/web/ConcentrationMgr.java
@@ -0,0 +1,8 @@
+package com.mydomain.sps.web;
+
+import org.jboss.seam.annotations.Name;
+
+@Name("concentrationMgr")
+public class ConcentrationMgr {
+
+}
