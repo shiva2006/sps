b3568b6d3f4945aaaa23009dfac0d9daad52f67b
shiva2006 <shiva.rsgr1@gmail.com>
2016-08-10 22:51:25 +0530

    methods added for student tab

--- sps/src/main/com/mydomain/sps/web/StudentMgr.java
+++ sps/src/main/com/mydomain/sps/web/StudentMgr.java
@@ -1,6 +1,7 @@
 package com.mydomain.sps.web;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.jboss.seam.ScopeType;
@@ -10,6 +11,7 @@ import org.jboss.seam.annotations.Scope;
 
 import com.mydomain.sps.entities.Student;
 import com.mydomain.sps.service.BaseDao;
+import com.mydomain.sps.service.Constants;
 
 @Name("studentMgr") @Scope(ScopeType.PAGE)
 public class StudentMgr extends BaseMgr {
@@ -19,20 +21,36 @@ public class StudentMgr extends BaseMgr {
 	 */
 	private static final long serialVersionUID = 1L;
 	@In BaseDao baseDaoImpl;
-	private Student student;	
+	private Student student = new Student();	
 	private List<Student> students = new ArrayList<Student>();
+	private String searchKey;
 	private boolean showStudnt;
+	private String temp;
 	
 	public void loadStudents() {
-		
+		searchKey =null;
+		students = baseDaoImpl.find(Constants.GET_ALL_STUDENTS);
 	}
 	
 	public void addStudent() {
-		
+		showStudnt=Boolean.TRUE;
+		student = new Student();
+	}
+	
+	public void closeDialog()
+	{
+		showStudnt = Boolean.FALSE;
 	}
 	
 	public void saveStudent() {
-		
+		student.setUpdatedBy(getLoggedUserId());		
+		student.setUpdatedOn(new Date());
+		student.setActive(Boolean.TRUE);
+		student.setCreatedBy(getLoggedUserId());
+		student.setCreatedOn(new Date());
+		baseDaoImpl.saveObject(student);
+		showStudnt=Boolean.FALSE;
+		loadStudents();
 	}
 
 	public Student getStudent() {
@@ -57,6 +75,22 @@ public class StudentMgr extends BaseMgr {
 
 	public void setShowStudnt(boolean showStudnt) {
 		this.showStudnt = showStudnt;
+	}
+
+	public String getSearchKey() {
+		return searchKey;
+	}
+
+	public void setSearchKey(String searchKey) {
+		this.searchKey = searchKey;
+	}
+
+	public String getTemp() {
+		return temp;
+	}
+
+	public void setTemp(String temp) {
+		this.temp = temp;
 	}
 	
 }
