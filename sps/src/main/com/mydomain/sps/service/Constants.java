270d0306b956f49469368bfc3886b15faed1b624
shiva2006 <shiva.rsgr1@gmail.com>
2016-08-10 22:48:40 +0530

    constant added for student

--- sps/src/main/com/mydomain/sps/service/Constants.java
+++ sps/src/main/com/mydomain/sps/service/Constants.java
@@ -4,6 +4,10 @@ public interface Constants {
 	public static final String GET_ALL_USERS="getAllUsers";
 	public static final String GET_SEARCHED_USER="getSearchUser";
 	public static final String GET_USERQ="getUserQ";
+
+	public static final String GET_ALL_STUDENTS="getAllStudents";
+
 	public static final String GET_ALL_CONCENTRATION = "getAllConcentration";
 	public static final String GET_ALL_ADVISORS = "getAllAdvisorUsers";
+
 }
